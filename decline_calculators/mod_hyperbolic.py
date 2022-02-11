e269c9d1d335bcd0cfb95290eb2c3b375148983f
Andrew Malone <adm0104@gmail.com>
2022-02-10 19:39:09 -0600

    Commented mod_hyperbolic.py

--- decline_calculators/mod_hyperbolic.py
+++ decline_calculators/mod_hyperbolic.py
@@ -3,27 +3,28 @@ from . import exponential as exp, hyperbolic as hyp
 from .. import helper_functions as helpers
 
 def calc_mod_hyperbolic_forecast(time_vector, **kwargs):
-    # Calculates hyperbolic decline rates and volumes, formatted for timeseries
+    # Calculates modified hyperbolic decline rates and volumes, formatted for timeseries
     # dataframe in case.py
     # INPUTS:
-    #   time_vector             Instantaneous times for forecast, in numpy array\
+    #   time_vector             Instantaneous times for forecast, in numpy array
     #   qi                      Initial rate
     #   Di                      Nominal initial decline rate
     #   b                       b-factor
+    #   Dt                      Terminal decline rate
     # OUTPUTS:
-    #   gas_forecast            Full rate forecast as an array - 1st column
+    #   forecast                Full rate forecast as an array - 1st column
     #                           contains entry rates, 2nd column contains exit rates,
     #                           3rd column contains cumulative production volumes
 
     qi, Di, b, Dt = kwargs['qi'], kwargs['Di'], kwargs['b'], kwargs['Dt']
     t_switch, q_switch = terminal_switch(qi, Di, b, Dt)
 
-    rate_vector_hyperbolic = hyp.calc_hyperbolic_rates(time_vector[time_vector <= t_switch], qi, Di, b)
-    rate_vector_exponential = exp.calc_exponential_rates(time_vector[time_vector > t_switch] - t_switch, q_switch, Dt)
-    rate_vector = np.concatenate([rate_vector_hyperbolic, rate_vector_exponential])
+    rate_vector_hyp = hyp.calc_hyperbolic_rates(time_vector[time_vector <= t_switch], qi, Di, b)
+    rate_vector_exp = exp.calc_exponential_rates(time_vector[time_vector > t_switch] - t_switch, q_switch, Dt)
+    rate_vector = np.concatenate([rate_vector_hyp, rate_vector_exp])
     entry_rates, exit_rates = helpers.vector_to_endpoints(rate_vector)
 
-    vols_vector = calc_mod_hyperbolic_volumes(time_vector[time_vector <= t_switch], t_switch, q_switch, rate_vector_hyperbolic, rate_vector_exponential, Di, Dt, b)
+    vols_vector = calc_mod_hyperbolic_volumes(time_vector[time_vector <= t_switch], rate_vector_hyp, rate_vector_exp, Di, Dt, b, t_switch, q_switch)
 
     return np.array([entry_rates, exit_rates, vols_vector]).transpose()
 
@@ -35,35 +36,42 @@ def calc_mod_hyperbolic_rates(time_vector, qi, Di, Dt, b, t_switch, q_switch):
     #   qi                      Initial rate
     #   Di                      Nominal initial decline rate
     #   b                       b-factor
+    #   t_switch                Time of switch from hyperbolic stem to exponential stem
+    #   q_switch                Instantaneous rate at time of switch between stems
     # OUTPUTS:
     #   rate_vector             Vector of rates
 
-    rate_vector_hyperbolic = hyp.calc_hyperbolic_rates(time_vector[time_vector <= t_switch], qi, Di, b)
-    rate_vector_exponential = exp.calc_exponential_rates(time_vector[time_vector > t_switch] - t_switch, q_switch, Dt)
-    return np.concatenate([rate_vector_hyperbolic, rate_vector_exponential])
+    rate_vector_hyp = hyp.calc_hyperbolic_rates(time_vector[time_vector <= t_switch], qi, Di, b)
+    rate_vector_exp = exp.calc_exponential_rates(time_vector[time_vector > t_switch] - t_switch, q_switch, Dt)
+    return np.concatenate([rate_vector_hyp, rate_vector_exp])
 
-def calc_mod_hyperbolic_volumes(time_vector, t_switch, q_switch, rate_vector_hyperbolic, rate_vector_exponential, Di, Dt, b):
-    # Calculates volumes integral for hyperbolic decline between rates in 
-    # rate_vector
+def calc_mod_hyperbolic_volumes(time_vector_hyp, rate_vector_hyp, rate_vector_exp, Di, Dt, b, t_switch, q_switch):
+    # Calculates volume integral for hyperbolic decline between rates in rate_vector_hyp and rate_vector_exp
+    # This function bridges the forecasts during the month where the switch occurs, calculating the sum
+    # of both the hyperbolic and exponential leg in that one month
     # INPUTS:
-    #   rate_vector             Instantaneous rates for forecast, in numpy array
+    #   time_vector_hyp         Instantaneous times for hyperbolic stem of forecast, in numpy array
+    #   rate_vector_hyp         Instantaneous rates for hyperbolic stem of forecast, in numpy array
+    #   rate_vector_exp         Instantaneous rates for exponential stem of forecast, in numpy array
     #   Di                      Nominal initial decline rate
     #   b                       b-factor
+    #   t_switch                Time of switch from hyperbolic stem to exponential stem
+    #   q_switch                Instantaneous rate at time of switch between stems
     # OUTPUTS:
     #   vols_vector             Vector of volumes
 
-    vols_vector_hyperbolic = hyp.calc_hyperbolic_volumes(time_vector, rate_vector_hyperbolic, Di, b)
+    vols_vector_hyperbolic = hyp.calc_hyperbolic_volumes(time_vector_hyp, rate_vector_hyp, Di, b)
 
-    switch_month_hyp_time = np.array([time_vector[-1], t_switch])
-    switch_month_hyp_rates = np.array([rate_vector_hyperbolic[-1], q_switch])
+    switch_month_hyp_time = np.array([time_vector_hyp[-1], t_switch])
+    switch_month_hyp_rates = np.array([rate_vector_hyp[-1], q_switch])
     switch_month_hyp_vols = hyp.calc_hyperbolic_volumes(switch_month_hyp_time, switch_month_hyp_rates, Di, b)
 
-    switch_month_exp_rates = np.array([q_switch, rate_vector_exponential[0]])
+    switch_month_exp_rates = np.array([q_switch, rate_vector_exp[0]])
     switch_month_exp_vols = exp.calc_exponential_volumes(switch_month_exp_rates, Dt)
 
     vol_switch = switch_month_hyp_vols + switch_month_exp_vols
 
-    vols_vector_exponential = exp.calc_exponential_volumes(rate_vector_exponential, Dt)
+    vols_vector_exponential = exp.calc_exponential_volumes(rate_vector_exp, Dt)
     return np.concatenate([vols_vector_hyperbolic, vol_switch, vols_vector_exponential])
 
 def calc_t_switch(Di, b, Dt):
